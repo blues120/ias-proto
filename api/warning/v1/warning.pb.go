551dcc2ca83c72895035077ec82ad731155fa25b
srd18521367458 <18521367458@189.cn>
2024-11-15 12:02:21 +0800

    2

--- api/warning/v1/warning.pb.go
+++ api/warning/v1/warning.pb.go
@@ -7,7 +7,7 @@
 package v1
 
 import (
-	v1 "gitlab.ctyuncdn.cn/ias/proto/api/standard/v1"
+	v1 "github.com/blues120/ias-proto/api/standard/v1"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
